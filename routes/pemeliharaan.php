09aa36ab90c7087f6ac197680ba273bf15ad95ae
benno25 <benno3811@gmail.com>
2024-01-31 17:45:26 +0700

    getData terbaru maximal 5, pemeliharaanController

--- routes/pemeliharaan.php
+++ routes/pemeliharaan.php
@@ -3,6 +3,7 @@ use App\Http\Controllers\PemeliharaanController;
 
 Route::get('/pemeliharaan', [PemeliharaanController::class, 'index']);
 Route::get('/pemeliharaan/indexAdmin', [PemeliharaanController::class, 'indexAdmin']);
+Route::get('/pemeliharaan/getData', [PemeliharaanController::class, 'getData']);
 Route::get('/pemeliharaan/indexTeknisi', [PemeliharaanController::class, 'indexTeknisi']);
 Route::get('/pemeliharaan/basedMonths', [PemeliharaanController::class, 'basedMonths']);
 Route::get('/pemeliharaan/show/{id}', [PemeliharaanController::class, 'show']);
